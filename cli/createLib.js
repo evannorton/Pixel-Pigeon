0c0feb7a7a36ff17df8b7a0e574fce07c7172329
Evan Norton <evan@retro-mmo.com>
2023-05-11 01:43:04 -0400

    library -> hot-reload

--- cli/createLib.js
+++ cli/createLib.js
@@ -1,7 +1,7 @@
 const { existsSync, mkdirSync, rmSync } = require("fs");
 const { join } = require("path");
 
-const paths = [join(__dirname, "..", "library-lib"), join(__dirname, "..", "game-lib")];
+const paths = [join(__dirname, "..", "hot-reload-lib"), join(__dirname, "..", "game-lib")];
 for (const path of paths) {
   if (existsSync(path)) {
     rmSync(path, { recursive: true });
