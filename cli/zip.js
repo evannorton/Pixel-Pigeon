6995f1991a4feee600c4326baac14bd04eb7557b
Evan Norton <evan@retro-mmo.com>
2023-05-22 03:07:09 -0400

    fix

--- cli/zip.js
+++ cli/zip.js
@@ -4,6 +4,6 @@ var archive = archiver('game.zip',{
   store: true
 });
 
-archive.directory('node_modules/pigeon-mode-game-library/out');
+archive.directory('node_modules/pigeon-mode-game-library/out', false);
 
 archive.finalize();
