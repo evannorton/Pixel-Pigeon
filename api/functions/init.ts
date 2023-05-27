19ab8824de740b0dddc1d884146705b5f1bed4fd
Evan Norton <evan@retro-mmo.com>
2023-05-27 04:53:45 -0400

    Init with canvas

--- api/functions/init.ts
+++ api/functions/init.ts
@@ -1,5 +1,22 @@
+import { Application, BaseTexture, SCALE_MODES, settings } from "pixi.js";
+import config from "../config.json";
+
 const init = (): void => {
   console.log("PMGL game initialized.");
+  settings.ROUND_PIXELS = true;
+  BaseTexture.defaultOptions.scaleMode= SCALE_MODES.NEAREST;
+  if (settings.RENDER_OPTIONS) {
+    settings.RENDER_OPTIONS.hello = false;
+  }
+  const app = new Application({
+    height: config.height,
+    width: config.width
+  });
+  app.renderer.view.addEventListener?.("contextmenu", (e: Event): void => {
+    e.preventDefault();
+  });
+  const screen = document.getElementById("screen");
+  screen?.appendChild(app.view as HTMLCanvasElement);
 };
 
 export default init;
--- cli/buildConfig.js
+++ cli/buildConfig.js
@@ -1,4 +1,4 @@
-const { writeFileSync } = require('fs');
+const { readFileSync, writeFileSync } = require('fs');
 const { join } = require('path');
 const { zodToTs, printNode } = require('zod-to-ts')
 const { configSchema } = require('./configSchema');
@@ -6,3 +6,7 @@ const { configSchema } = require('./configSchema');
 const { node } = zodToTs(configSchema, 'Config')
 
 writeFileSync(join(__dirname, "..", "api", "interfaces", "Config.ts"), `export default interface Config ${printNode(node)}`);
+
+const configString = readFileSync(join("pmgl.json")).toString();
+
+writeFileSync(join(__dirname, "..", "api", "config.json"), configString);
