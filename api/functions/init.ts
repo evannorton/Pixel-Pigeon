08541bd284444b14e132af0d00506f9d08bc86e5
Evan Norton <evan@retro-mmo.com>
2023-05-27 06:08:36 -0400

    eslint

--- api/functions/init.ts
+++ api/functions/init.ts
@@ -4,13 +4,13 @@ import config from "../config.json";
 const init = (): void => {
   console.log("PMGL game initialized.");
   settings.ROUND_PIXELS = true;
-  BaseTexture.defaultOptions.scaleMode= SCALE_MODES.NEAREST;
+  BaseTexture.defaultOptions.scaleMode = SCALE_MODES.NEAREST;
   if (settings.RENDER_OPTIONS) {
     settings.RENDER_OPTIONS.hello = false;
   }
   const app = new Application({
     height: config.height,
-    width: config.width
+    width: config.width,
   });
   app.renderer.view.addEventListener?.("contextmenu", (e: Event): void => {
     e.preventDefault();
