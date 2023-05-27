5034056f31225418b3fc5dcebb54f8e32ff21f03
Evan Norton <evan@retro-mmo.com>
2023-05-27 08:07:47 -0400

    generate source text

--- cli/configSchema.js
+++ cli/configSchema.js
@@ -2,4 +2,4 @@ const { generate } = require("ts-to-zod");
 const { readFileSync } = require("fs");
 const { join } = require("path");
 
-module.exports.configSchema = generate(readFileSync(join(__dirname, "..", "api", "interfaces", "Config.ts")).toString());
+module.exports.configSchema = generate({ sourceText: readFileSync(join(__dirname, "..", "api", "interfaces", "Config.ts")).toString() });
