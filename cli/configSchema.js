62a79bd8802a63601cd0f4646573a889fb7cf607
Evan Norton <evan@retro-mmo.com>
2023-05-27 08:02:36 -0400

    Add missing require

--- cli/configSchema.js
+++ cli/configSchema.js
@@ -1,3 +1,4 @@
 const { generate } = require("ts-to-zod");
+const { readFileSync } = require("fs");
 
 module.exports.configSchema = generate(readFileSync(join(__dirname, "..", "api", "interfaces", "Config.ts")).toString());
