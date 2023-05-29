34391e8e07beaa49ff673a8864f9d513c65e2839
Evan Norton <evan@retro-mmo.com>
2023-05-29 02:19:29 -0400

    sort

--- api/functions/init.ts
+++ api/functions/init.ts
@@ -5,23 +5,37 @@ import state from "pigeon-mode-game-library/api/state";
 import tick from "pigeon-mode-game-library/api/functions/tick";
 
 const init = (): void => {
+
   console.log("PMGL game initialized.");
+
+  const screen = document.getElementById("screen");
+
   settings.ROUND_PIXELS = true;
   BaseTexture.defaultOptions.scaleMode = SCALE_MODES.NEAREST;
   if (settings.RENDER_OPTIONS) {
     settings.RENDER_OPTIONS.hello = false;
   }
+
+  addEventListener("resize", sizeScreen);
   app.renderer.view.addEventListener?.("contextmenu", (e: Event): void => {
     e.preventDefault();
   });
-  app.ticker.add(tick);
-  const screen = document.getElementById("screen");
+  screen?.addEventListener("mousedown", () => {
+   if (!state.hasInteracted) {
+    state.hasInteracted = true;
+   }
+  });
+
   screen?.appendChild(app.view as HTMLCanvasElement);
+
   sizeScreen();
-  addEventListener("resize", sizeScreen);
+  
   Assets.load("./fonts/RetroPixels.fnt").then((): void => {
     state.loadedAssets++;
   });
+
+  app.ticker.add(tick);
+
 };
 
 export default init;
