0fb7758f28a5ab63c89a246b743efb3d74f1eddd
Evan Norton <evan@retro-mmo.com>
2023-06-23 02:53:41 -0400

    eslint

--- api/functions/update.ts
+++ api/functions/update.ts
@@ -6,19 +6,36 @@ const update = (): void => {
   if (state.values.hasInteracted) {
     navigator.getGamepads().forEach((gamepad: Gamepad | null): void => {
       if (gamepad) {
-        gamepad.buttons.forEach((button: GamepadButton, buttonIndex: number): void => {
-          if (!state.values.heldGamepadButtons.includes(buttonIndex) && !!button.pressed) {
-            state.setValues({ heldGamepadButtons: [...state.values.heldGamepadButtons, buttonIndex] });
-            getDefinables(InputHandler).forEach((inputHandler): void => {
-              inputHandler.handleGamepadButton(buttonIndex);
-            });
+        gamepad.buttons.forEach(
+          (button: GamepadButton, buttonIndex: number): void => {
+            if (
+              !state.values.heldGamepadButtons.includes(buttonIndex) &&
+              !!button.pressed
+            ) {
+              state.setValues({
+                heldGamepadButtons: [
+                  ...state.values.heldGamepadButtons,
+                  buttonIndex,
+                ],
+              });
+              getDefinables(InputHandler).forEach((inputHandler): void => {
+                inputHandler.handleGamepadButton(buttonIndex);
+              });
+            } else if (
+              state.values.heldGamepadButtons.includes(buttonIndex) &&
+              !button.pressed
+            ) {
+              state.setValues({
+                heldGamepadButtons: state.values.heldGamepadButtons.filter(
+                  (gamepadButtonIndex: number): boolean =>
+                    buttonIndex !== gamepadButtonIndex
+                ),
+              });
+            }
           }
-          else if (state.values.heldGamepadButtons.includes(buttonIndex) && !button.pressed) {
-            state.setValues({ heldGamepadButtons: state.values.heldGamepadButtons.filter((gamepadButtonIndex: number): boolean => buttonIndex !== gamepadButtonIndex) });
-          }
-        });
+        );
       }
-    }); 
+    });
   }
 };
 
