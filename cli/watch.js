0ae92cd232952a2df467ae0f0a05c6352389075c
Evan Norton <evan@retro-mmo.com>
2023-06-23 03:28:40 -0400

    Watch for changes to .ogmo files

--- cli/watch.js
+++ cli/watch.js
@@ -11,7 +11,7 @@ writeFileSync(join(__dirname, "watchExecCompleted.json"), JSON.stringify(false))
 const watcher = nodemon({
   delay: 1,
   exec: "node ./node_modules/pigeon-mode-game-library/cli/handleWatch || exit 1",
-  ext: "css,fnt,gif,js,json,mp3,mustache,png,ts,ttf",
+  ext: "css,fnt,gif,js,json,ogmo,mp3,mustache,png,ts,ttf",
   stdout: true,
   stderr: true,
   watch: [
