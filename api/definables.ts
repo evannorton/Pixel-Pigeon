bceecc2eec19f0902e08b718b672760d69b672eb
Evan Norton <evan@retro-mmo.com>
2023-06-24 14:31:28 -0400

    expose definables to dev console

--- api/definables.ts
+++ api/definables.ts
@@ -1,5 +1,10 @@
 import Definable from "./classes/Definable";
 
 const definables: Map<string, Map<string, Definable>> = new Map();
+(
+  window as unknown as {
+    definables: Map<string, Map<string, Definable>>;
+  }
+).definables = definables;
 
 export default definables;
