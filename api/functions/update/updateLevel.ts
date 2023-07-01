471ccaef750cfd6d8a4a60ac2e6526ffc213292c
Evan Norton <evan@retro-mmo.com>
2023-07-01 12:26:06 -0400

    Normalize diagonal entity movement

--- api/functions/update/updateLevel.ts
+++ api/functions/update/updateLevel.ts
@@ -26,8 +26,38 @@ const updateLevel = (): void => {
   }
   for (const layer of level.layers) {
     for (const entity of layer.entities) {
-      entity.x += entity.xVelocity * (state.values.app.ticker.deltaMS / 1000);
-      entity.y += entity.yVelocity * (state.values.app.ticker.deltaMS / 1000);
+      const unnormalizedEntityX: number =
+        entity.x + entity.xVelocity * (state.values.app.ticker.deltaMS / 1000);
+      const unnormalizedEntityY: number =
+        entity.y + entity.yVelocity * (state.values.app.ticker.deltaMS / 1000);
+      const nextX: number =
+        entity.xVelocity > 0
+          ? Math.ceil(unnormalizedEntityX)
+          : Math.floor(unnormalizedEntityX);
+      const nextY: number =
+        entity.yVelocity > 0
+          ? Math.ceil(unnormalizedEntityY)
+          : Math.floor(unnormalizedEntityY);
+      const xDistanceToNextX: number =
+        entity.xVelocity > 0
+          ? nextX - unnormalizedEntityX
+          : unnormalizedEntityX - nextX;
+      const yDistanceToNextY: number =
+        entity.yVelocity > 0
+          ? nextY - unnormalizedEntityY
+          : unnormalizedEntityY - nextY;
+      const averageDistanceToNext: number =
+        (xDistanceToNextX + yDistanceToNextY) / 2;
+      if (entity.xVelocity > 0) {
+        entity.x = nextX - averageDistanceToNext;
+      } else {
+        entity.x = nextX + averageDistanceToNext;
+      }
+      if (entity.yVelocity > 0) {
+        entity.y = nextY - averageDistanceToNext;
+      } else {
+        entity.y = nextY + averageDistanceToNext;
+      }
       entity.xVelocity = 0;
       entity.yVelocity = 0;
     }
