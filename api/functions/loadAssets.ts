b5e6f15afd353fdeffe6df69d4b450a083a6ab28
Evan Norton <evan@retro-mmo.com>
2023-07-16 05:44:26 -0400

    resolve eslint error

--- api/functions/loadAssets.ts
+++ api/functions/loadAssets.ts
@@ -3,7 +3,9 @@ import getDefinables from "./getDefinables";
 import loadPixiAsset from "./loadPixiAsset";
 
 const loadAssets = (): void => {
-  loadPixiAsset("fonts/RetroPixels.fnt");
+  loadPixiAsset("fonts/RetroPixels.fnt").catch((error: Error): void => {
+    throw error;
+  });
   getDefinables(ImageSource).forEach((imageSource: ImageSource): void => {
     imageSource.loadTexture();
   });
