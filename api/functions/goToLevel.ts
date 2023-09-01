d94651c37e10da266681bdca7025d326e30071f5
DataEncoded <dataencoded@gmail.com>
2023-09-01 13:54:07 -0500

    [docs] Add goToLevel documentation

--- api/functions/goToLevel.ts
+++ api/functions/goToLevel.ts
@@ -1,5 +1,16 @@
 import { state } from "pigeon-mode-game-framework/api/state";
 
+/**
+ * Switches the current active level to the provided level, levels must be made in LDTK
+ * 
+ * @param levelID - A string of the ID of the level as defined in LDTK
+ * 
+ * @example
+ * Changes the level to the default name level of LDTK
+ * ```ts
+ * goToLevel("level_0");
+ * ```
+ */
 export const goToLevel = (levelID: string): void => {
   state.setValues({ levelID });
 };
