36a49757be596d15bb60fdec1845306d786bdb75
DataEncoded <dataencoded@gmail.com>
2023-09-02 04:33:39 -0500

    [docs] Document onTick

--- api/functions/onTick.ts
+++ api/functions/onTick.ts
@@ -1,5 +1,9 @@
 import { state } from "pigeon-mode-game-framework/api/state";
 
+/**
+ * Function adds any callbacks to a list of callbacks that are called every tick
+ * @param callback - Callback to be added to the list of functions that are called every tick
+ */
 export const onTick = (callback: () => void): void => {
   state.setValues({
     onTickCallbacks: [...state.values.onTickCallbacks, callback],
