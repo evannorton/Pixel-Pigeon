3b2bc9e437d1a813be31250dfd790d5d632be43d
DataEncoded <dataencoded@gmail.com>
2023-09-02 14:17:44 -0500

    [docs] Document EntityCollidable

--- api/types/EntityCollidable.ts
+++ api/types/EntityCollidable.ts
@@ -1,4 +1,19 @@
+/**
+ * Stores information about Entity collisions.
+ * Setting up collisions is done with {@link SpawnEntityOptions.onOverlap}.
+ * 
+ * @remarks
+ * If you set up collisions, the data in this interface is the data of the entity your are colliding with.
+ * For example if you set up {@link SpawnEntityOptions.onOverlap | onOverlap} on a player entity,
+ * any overlapData you recieve will be the data of entities the player has collided with, but will not include player collision data.
+ */
 export interface EntityCollidable<CollisionLayer extends string> {
+  /**
+   * String CollisionLayer that the collided entity is apart of
+   */
   readonly collisionLayer: CollisionLayer;
+  /**
+   * String entityID of the collided entity
+   */
   readonly entityID: string;
 }
