b8cca1397eeaf3ee18e8115508adde4f5ebc03a3
DataEncoded <dataencoded@gmail.com>
2023-09-02 04:27:20 -0500

    [docs] Cleanup getInputTickHandlerGroupID

--- api/functions/getInputTickHandlerGroupID.ts
+++ api/functions/getInputTickHandlerGroupID.ts
@@ -2,7 +2,7 @@ import { InputTickHandler } from "pigeon-mode-game-framework/api/classes/InputTi
 import { getDefinable } from "pigeon-mode-game-framework/api/functions/getDefinable";
 
 /**
- * Gets the string GroupID that was used to create the {@link createInputTickHandler | InputTickHandler} by supplying it's string ID
+ * Gets the string GroupID that was used to create the {@link createInputTickHandler | InputTickHandler} by supplying the Handler's StringID
  * @param inputTickHandlerID - The string inputTickHandlerID of the InputTickHandler
  * @returns A string GroupID of the supplied InputTickHandlerGroup if it exists
  */
