ec7e301e987ce6dae24dfa2f0f9eac275e193b22
Evan Norton <evan@retro-mmo.com>
2023-09-11 08:08:58 -0400

    Improve check for node_modules path

--- cli/nodeModulesPath.js
+++ cli/nodeModulesPath.js
@@ -5,8 +5,8 @@ const getNodeModulesPath = () => {
   if (existsSync(join(__dirname, "..", "node_modules"))) {
     return join(__dirname, "..", "node_modules");
   }
-  if (existsSync(join(__dirname, "..", "..", "..", "node_modules"))) {
-    return join(__dirname, "..", "..", "..", "node_modules");
+  if (existsSync(join(resolve(), "node_modules"))) {
+    return join(resolve(), "node_modules");
   }
   throw new Error("Could not get node_modules path.");
 }
