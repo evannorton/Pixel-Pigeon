9c609d76450627104bb092c2dd13d1c121dacd23
Evan Norton <evan@retro-mmo.com>
2023-11-30 02:43:43 -0500

    Fix lint error count

--- cli/lint.js
+++ cli/lint.js
@@ -7,12 +7,14 @@ const eslint = new ESLint({
 });
 
 eslint.lintFiles("./src").then((results) => {
+  let errorCount = 0;
   for (const result of results) {
     for (const message of result.messages) {
+      errorCount++;
       console.error(`${result.filePath}:${message.line}:${message.column} ${message.message} (${message.ruleId})`);
     }
   }
-  if (results.length > 0) {
+  if (errorCount > 0) {
     process.exit(1);
   }
 });
--- cli/lintFix.js
+++ cli/lintFix.js
@@ -9,12 +9,14 @@ const eslint = new ESLint({
 
 eslint.lintFiles("./src").then((results) => {
   ESLint.outputFixes(results).then(() => {
+    let errorCount = 0;
     for (const result of results) {
       for (const message of result.messages) {
+        errorCount++;
         console.error(`${result.filePath}:${message.line}:${message.column} ${message.message} (${message.ruleId})`);
       }
     }
-    if (results.length > 0) {
+    if (errorCount > 0) {
       process.exit(1);
     }
   });
