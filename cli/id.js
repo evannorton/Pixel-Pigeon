8335292ce9fb6a68a40fb936e97f8927d0140eb6
Evan Norton <evan@retro-mmo.com>
2024-01-24 04:39:32 -0500

    No hyphens in id

--- cli/id.js
+++ cli/id.js
@@ -1,8 +1,48 @@
-import("nanoid").then(({nanoid}) => {
+import("nanoid").then(({ customAlphabet }) => {
   const { writeFileSync } = require("fs");
   const { join, resolve } = require("path");
-  
-  const id = nanoid();
-  
+
+  validIDCharacters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "Z",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+  ];
+
+  const id = customAlphabet(validIDCharacters.join(""), 21)();
+
   writeFileSync(join(resolve(), "pp-id.json"), JSON.stringify(id));
 })
