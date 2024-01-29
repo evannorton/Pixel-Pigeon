1489ce2b8ea821914ceff67aca73805be308fc03
Evan Norton <evan@retro-mmo.com>
2024-01-29 03:16:20 -0500

    Revert moving away from nodemon

--- cli/watch.js
+++ cli/watch.js
@@ -1,85 +1,51 @@
 const killPort = require("kill-port");
-const { watch, readFileSync, writeFileSync } = require("fs");
+const nodemon = require("nodemon");
+const { readFileSync, writeFileSync, existsSync } = require("fs");
 const { join, resolve, sep } = require("path");
-const { debounce } = require("throttle-debounce");
-const { spawn } = require("child_process");
 
 const build = require("./buildCommand");
 
 writeFileSync(join(__dirname, "watchExec.json"), JSON.stringify([build]));
-writeFileSync(
-  join(__dirname, "watchExecCompleted.json"),
-  JSON.stringify(false)
-);
+writeFileSync(join(__dirname, "watchExecCompleted.json"), JSON.stringify(false));
 
-const extensions = [
-  "css",
-  "fnt",
-  "gif",
-  "js",
-  "json",
-  "mp3",
-  "mustache",
-  "png",
-  "ts",
-  "ttf",
-  "json",
-  "ldtk",
-];
-
-watch(".", { recursive: true }, (_, filename) => {
-  if (filename !== null) {
-    const split = filename.split(".");
-    const ext = split[split.length - 1];
-    if (filename.startsWith(`node_modules${sep}pixel-pigeon`)) return;
-    if (extensions.includes(ext)) {
-      addFile(filename);
-    }
-  }
+const watcher = nodemon({
+  delay: 1,
+  exec: `node ${join(__dirname, "handleWatch")} || exit 1`,
+  ext: "css,fnt,gif,js,json,mp3,mustache,png,ts,ttf,json,ldtk",
+  stdout: true,
+  stderr: true,
+  watch: [
+    "./src/",
+    "./audio/",
+    "./images/",
+    "./node-modules/",
+    "./package.json",
+    "./package-lock.json",
+    "./pp-config.json",
+    "./pp-dev.json",
+    "./pp-env.json",
+    "./project.ldtk"
+  ]
 });
 
-const hardRestart = () => {
-  handler.kill();
-  process.exit();
-};
-
-const start = () => {
-  const command = spawn("node", [join(__dirname, "handleWatch")]);
-
-  command.stdout.on("data", (data) => {
-    console.log(data.toString());
-  });
-
-  command.stderr.on("data", (data) => {
-    console.error(data.toString());
-  });
-  return command;
-};
-let handler = start();
-
-const restart = () => {
-  console.warn("Restarting due to file changes...");
-  setTimeout(() => {
-    killPort(3000)
-      .catch(() => { })
-      .finally(() => {
-        handler.kill();
-        handler = start();
-      });
-  }, 0);
-};
-
-let files = [];
-
-const addFile = (file) => {
-  files.push(file);
-  batchedReloading();
-};
-
-const batchedReloading = debounce(1000, () => {
-  restart();
+watcher.addListener("restart", (files) => {
   const absoluteDirPieces = resolve().split(sep);
   absoluteDirPieces[0] = absoluteDirPieces[0].toLowerCase();
+
+  if (typeof files === "undefined") {
+    files = [];
+  }
+
+  if (existsSync(join(__dirname, "watchModifiedFiles.json"))) {
+    const modifiedFiles = JSON.parse(readFileSync(join(__dirname, "watchModifiedFiles.json")).toString());
+    modifiedFiles.forEach((file) => {
+      if (files.includes(file) === false) {
+        files.push(file);
+      }
+    });
+  }
+
+  writeFileSync(join(__dirname, "watchModifiedFiles.json"), JSON.stringify(files));
 
   const filesPieces = [];
 
@@ -89,7 +55,8 @@ const batchedReloading = debounce(1000, () => {
     for (const absoluteDirPiece of absoluteDirPieces) {
       if (absoluteDirPiece === filePieces[0]) {
         filePieces.shift();
-      } else {
+      }
+      else {
         break;
       }
     }
@@ -97,7 +64,7 @@ const batchedReloading = debounce(1000, () => {
   });
 
   const buildRelatedFilePieces = filesPieces.find((filePieces) => {
-    const joinedFilePieces = filePieces.join(sep);
+    const joinedFilePieces = filePieces.join("/");
     if (filePieces[0] === "node_modules") {
       return true;
     }
@@ -120,36 +87,25 @@ const batchedReloading = debounce(1000, () => {
   });
 
   if (typeof buildRelatedFilePieces !== "undefined") {
-    console.log(
-      `Exiting dev script because of a change to ${buildRelatedFilePieces.join(
-        "/"
-      )}.`
-    );
-    hardRestart();
-  } else {
+    console.log(`Exiting dev script because of a change to ${buildRelatedFilePieces.join("/")}.`);
+    watcher.emit("quit");
+  }
+  else {
     const commands = [];
 
     if (
-      JSON.parse(
-        readFileSync(join(__dirname, "watchExecCompleted.json")).toString()
-      ) === false ||
-      filesPieces.some((filePieces) => {
+      JSON.parse(readFileSync(join(__dirname, "watchExecCompleted.json")).toString()) === false
+      || filesPieces.some((filePieces) => {
         const joinedFilePieces = filePieces.join("/");
         if (filePieces[0] === "src") {
-          return (
-            joinedFilePieces.substring(joinedFilePieces.length - 3) === ".ts" ||
-            joinedFilePieces.substring(joinedFilePieces.length - 5) === ".json"
-          );
+          return joinedFilePieces.substring(joinedFilePieces.length - 3) === ".ts"
+            || joinedFilePieces.substring(joinedFilePieces.length - 5) === ".json";
         }
         if (filePieces[0] === "audio") {
-          return (
-            joinedFilePieces.substring(joinedFilePieces.length - 4) === ".mp3"
-          );
+          return joinedFilePieces.substring(joinedFilePieces.length - 4) === ".mp3"
         }
         if (filePieces[0] === "images") {
-          return (
-            joinedFilePieces.substring(joinedFilePieces.length - 4) === ".png"
-          );
+          return joinedFilePieces.substring(joinedFilePieces.length - 4) === ".png"
         }
         if (joinedFilePieces === "style.css") {
           return true;
@@ -161,11 +117,17 @@ const batchedReloading = debounce(1000, () => {
       })
     ) {
       commands.push(build);
-    } else {
-      commands.push("node ./node_modules/pixel-pigeon/cli/buildHTML");
+    }
+    else {
+      commands.push("node ./node_modules/pixel-pigeon/cli/buildHTML")
     }
 
     writeFileSync(join(__dirname, "watchExec.json"), JSON.stringify(commands));
-    files = [];
   }
+});
+
+watcher.addListener("quit", () => {
+  killPort(3000).then(() => {
+    process.exit();
+  });
 });
