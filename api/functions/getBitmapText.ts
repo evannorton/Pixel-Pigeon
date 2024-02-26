dbe1a0613fb18556384115643716cc890e0dcd2a
Evan Norton <evan@retro-mmo.com>
2024-02-26 03:41:44 -0500

    Remove text asterisk code

--- api/functions/getBitmapText.ts
+++ api/functions/getBitmapText.ts
@@ -1,7 +1,5 @@
 import { BitmapText, TextStyleAlign } from "pixi.js";
 
-const regex: RegExp = new RegExp("\\*", "gu");
-
 export const getBitmapText = (
   text: string,
   color: string,
@@ -10,8 +8,7 @@ export const getBitmapText = (
   maxLines: number,
   horizontalAlignment: TextStyleAlign,
 ): BitmapText => {
-  const replaced: string = text.replace(regex, "");
-  const bitmapText: BitmapText = new BitmapText(replaced, {
+  const bitmapText: BitmapText = new BitmapText(text, {
     align: horizontalAlignment,
     fontName: "RetroPixels",
     fontSize: size * 16,
@@ -25,70 +22,8 @@ export const getBitmapText = (
     return bitmapText;
   }
   bitmapText.destroy();
-  const indices: number[] = [];
-  text.split("").forEach((char: string, key: number): void => {
-    if (char === "*") {
-      indices.push(key);
-    }
-  });
-  const asterisks: {
-    index: number;
-    word: string;
-  }[] = [];
-  indices.forEach((index: number): void => {
-    const fragment: string = text.substring(0, index);
-    const wordStart: number = Math.max(0, fragment.lastIndexOf(" "));
-    asterisks.push({
-      index,
-      word: fragment.substring(wordStart),
-    });
-  });
-  const sortedAsterisks: {
-    index: number;
-    word: string;
-  }[] = [...asterisks].sort(
-    (
-      asterisk1: {
-        index: number;
-        word: string;
-      },
-      asterisk2: {
-        index: number;
-        word: string;
-      },
-    ): number => {
-      const word1BitmapText: BitmapText = new BitmapText(asterisk1.word, {
-        align: horizontalAlignment,
-        fontName: "RetroPixels",
-        fontSize: size * 16,
-      });
-      const word2BitmapText: BitmapText = new BitmapText(asterisk2.word, {
-        align: horizontalAlignment,
-        fontName: "RetroPixels",
-        fontSize: size * 16,
-      });
-      const diff: number = word2BitmapText.width - word1BitmapText.width;
-      word1BitmapText.destroy();
-      word2BitmapText.destroy();
-      return diff;
-    },
-  );
-  const pieces: string[] = text.split("");
-  if (sortedAsterisks.length > 0) {
-    const asterisk: {
-      index: number;
-      word: string;
-    } = sortedAsterisks[0];
-    pieces.splice(asterisk.index - 1, 1);
-  }
-  const joined: string = pieces.join("");
-  if (joined === text) {
-    throw new Error(
-      `Text "${text}" of max width ${maxWidth} and lines ${maxLines} does not fit.`,
-    );
-  }
   return getBitmapText(
-    joined,
+    text,
     color,
     size,
     maxWidth,
