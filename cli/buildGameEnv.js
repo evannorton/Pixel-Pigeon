34f2de4bb6c73a77f605fc1cb9d851065cdbb67b
Evan Norton <evan@retro-mmo.com>
2024-04-19 22:26:18 -0400

    Load in github env vars

--- cli/buildGameEnv.js
+++ cli/buildGameEnv.js
@@ -1,12 +1,17 @@
 const gameEnv = {};
-console.log(process.env);
+
 require('dotenv').config({
   processEnv: gameEnv
 });
+
+if (process.env.GITHUB_ENV_VARS) {
+  gameEnv = { ...gameEnv, ...JSON.parse(process.env.GITHUB_ENV_VARS) }
+}
+
 const { writeFileSync } = require("fs");
 const { join, resolve } = require("path");
 
-console.log("yo");
+console.log(process.env);
 console.log(gameEnv);
 
 writeFileSync(join(resolve(), "out", "game-env.json"), JSON.stringify(gameEnv));
