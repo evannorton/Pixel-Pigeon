d7726040d5ea1d067e630664ed967bf59f45dc87
Evan Norton <evan@retro-mmo.com>
2024-04-19 22:38:22 -0400

    Remove logs

--- cli/buildGameEnv.js
+++ cli/buildGameEnv.js
@@ -11,7 +11,4 @@ if (process.env.GITHUB_ENV_VARS) {
 const { writeFileSync } = require("fs");
 const { join, resolve } = require("path");
 
-console.log(process.env);
-console.log(gameEnv);
-
 writeFileSync(join(resolve(), "out", "game-env.json"), JSON.stringify(gameEnv));
