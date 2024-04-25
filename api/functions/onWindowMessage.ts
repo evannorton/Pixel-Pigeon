26b4443ac9d4efb9a80545e2c63641705edea944
Evan Norton <evan@retro-mmo.com>
2024-04-25 05:05:54 -0400

    Remove logs

--- api/functions/onWindowMessage.ts
+++ api/functions/onWindowMessage.ts
@@ -1,7 +1,5 @@
 export const onWindowMessage = (callback: (message: unknown) => void): void => {
   window.addEventListener("message", (messageEvent: MessageEvent): void => {
-    console.log("on message");
-    console.log(messageEvent);
     callback(messageEvent.data);
   });
 };
