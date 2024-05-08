3ea98e00115e57011c050ee5fab3062da5085d5d
Evan Norton <evan@retro-mmo.com>
2024-05-08 01:38:17 -0400

    Only trigger onWindowMessage for messages from parent

--- api/functions/onWindowMessage.ts
+++ api/functions/onWindowMessage.ts
@@ -1,5 +1,7 @@
 export const onWindowMessage = (callback: (message: unknown) => void): void => {
   window.addEventListener("message", (messageEvent: MessageEvent): void => {
-    callback(messageEvent.data);
+    if (messageEvent.source === window.parent) {
+      callback(messageEvent.data);
+    }
   });
 };
