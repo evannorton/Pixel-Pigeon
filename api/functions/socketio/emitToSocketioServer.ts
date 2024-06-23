2c1be1b07b85a02dafb46c04ea3eaf7995080e09
Evan Norton <evan@retro-mmo.com>
2024-06-23 00:56:17 -0400

    Change names of generic types for socket messages

--- api/functions/socketio/emitToSocketioServer.ts
+++ api/functions/socketio/emitToSocketioServer.ts
@@ -1,11 +1,11 @@
 import { state } from "../../state";
 
-export interface EmitToSocketIOServerOptions<Request extends object> {
-  data: Request;
+export interface EmitToSocketIOServerOptions<Message extends object> {
+  data: Message;
   event: string;
 }
-export const emitToSocketioServer = <Request extends object>(
-  options: EmitToSocketIOServerOptions<Request>,
+export const emitToSocketioServer = <Message extends object>(
+  options: EmitToSocketIOServerOptions<Message>,
 ): void => {
   if (state.values.socket === null) {
     throw new Error("Attempted to emit to socket.io server with no socket.");
--- api/functions/socketio/listenToSocketioEvent.ts
+++ api/functions/socketio/listenToSocketioEvent.ts
@@ -1,16 +1,16 @@
 import { state } from "../../state";
 
-export interface ListenToSocketioEventOptions<Update extends object> {
+export interface ListenToSocketioEventOptions<Message extends object> {
   event: string;
-  onMessage: (message: Update) => void;
+  onMessage: (message: Message) => void;
 }
-export const listenToSocketioEvent = <Update extends object>(
-  options: ListenToSocketioEventOptions<Update>,
+export const listenToSocketioEvent = <Message extends object>(
+  options: ListenToSocketioEventOptions<Message>,
 ): void => {
   if (state.values.socket === null) {
     throw new Error("Attempted to listen to socket.io event with no socket.");
   }
-  state.values.socket.on(options.event, (message: Update): void => {
+  state.values.socket.on(options.event, (message: Message): void => {
     options.onMessage(message);
   });
 };
