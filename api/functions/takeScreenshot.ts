80077c934e67702972d1b00fa12b5a7335f38f4c
Evan Norton <evan@retro-mmo.com>
2026-05-01 13:14:11 -0500

    Remove unnecessary undefined check

--- api/functions/takeScreenshot.ts
+++ api/functions/takeScreenshot.ts
@@ -31,65 +31,60 @@ export const takeScreenshot = (): void => {
     copyScreenshotToggleInputElement instanceof HTMLInputElement &&
     scaleScreenshotInputElement instanceof HTMLInputElement
   ) {
-    if (typeof navigator.clipboard === "undefined") {
-      toast.error("Failed to take screenshot");
+    const canvas: ICanvas = state.values.app.renderer.extract.canvas(
+      state.values.app.stage,
+      new Rectangle(
+        0,
+        0,
+        state.values.config.width,
+        state.values.config.height,
+      ),
+    );
+    if (
+      typeof canvas.toDataURL === "undefined" ||
+      typeof canvas.toBlob === "undefined"
+    ) {
+      toast.error("Failed to take a screenshot");
     } else {
-      const canvas: ICanvas = state.values.app.renderer.extract.canvas(
-        state.values.app.stage,
-        new Rectangle(
+      const scale: number = Number(scaleScreenshotInputElement.value);
+      const context: ICanvasRenderingContext2D | null = canvas.getContext("2d");
+      if (context !== null) {
+        const imageData: ImageData = context.getImageData(
           0,
           0,
-          state.values.config.width,
-          state.values.config.height,
-        ),
-      );
-      if (
-        typeof canvas.toDataURL === "undefined" ||
-        typeof canvas.toBlob === "undefined"
-      ) {
-        toast.error("Failed to take a screenshot");
+          canvas.width,
+          canvas.height,
+        );
+        canvas.width *= scale;
+        canvas.height *= scale;
+        context.putImageData(imageData, 0, 0);
+        context.scale(scale, scale);
+        context.imageSmoothingEnabled = false;
+        context.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+      }
+      if (copyScreenshotToggleInputElement.checked === false) {
+        const anchor: HTMLAnchorElement = document.createElement("a");
+        anchor.download = `${state.values.config.name} screenshot.png`;
+        anchor.href = canvas.toDataURL();
+        anchor.click();
+        toast.success("Screenshot saved");
       } else {
-        const scale: number = Number(scaleScreenshotInputElement.value);
-        const context: ICanvasRenderingContext2D | null =
-          canvas.getContext("2d");
-        if (context !== null) {
-          const imageData: ImageData = context.getImageData(
-            0,
-            0,
-            canvas.width,
-            canvas.height,
-          );
-          canvas.width *= scale;
-          canvas.height *= scale;
-          context.putImageData(imageData, 0, 0);
-          context.scale(scale, scale);
-          context.imageSmoothingEnabled = false;
-          context.drawImage(canvas, 0, 0, canvas.width, canvas.height);
-        }
-        if (copyScreenshotToggleInputElement.checked === false) {
-          const anchor: HTMLAnchorElement = document.createElement("a");
-          anchor.download = `${state.values.config.name} screenshot.png`;
-          anchor.href = canvas.toDataURL();
-          anchor.click();
-          toast.success("Screenshot saved");
-        } else {
-          canvas.toBlob((blob: Blob | null): void => {
-            if (blob !== null) {
-              navigator.clipboard
-                .write([new ClipboardItem({ "image/png": blob })])
-                .then((): void => {
-                  toast.success("Screenshot copied to clipboard");
-                })
-                .catch((clipboardError: unknown): void => {
-                  toast.error("Failed to copy screenshot to clipboard");
-                  console.error(
-                    "Failed to copy screenshot to clipboard",
-                    clipboardError,
-                  );
-                });
-            }
-          });
-        }
+        canvas.toBlob((blob: Blob | null): void => {
+          if (blob !== null) {
+            navigator.clipboard
+              .write([new ClipboardItem({ "image/png": blob })])
+              .then((): void => {
+                toast.success("Screenshot copied to clipboard");
+              })
+              .catch((clipboardError: unknown): void => {
+                toast.error("Failed to copy screenshot to clipboard");
+                console.error(
+                  "Failed to copy screenshot to clipboard",
+                  clipboardError,
+                );
+              });
+          }
+        });
       }
     }
   } else {
